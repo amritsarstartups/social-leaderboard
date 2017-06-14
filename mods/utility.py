1a95aea6f3cc15468904057a8a3e295b27a7850a
parassharmaa <paraazz@outlook.com>
2017-06-14 12:08:15 +0530

    Update: tweet_fetch complete, minor bugs fixed

--- mods/utility.py
+++ mods/utility.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def set_password(password):
-    return pw_hash = generate_password_hash(password)
+    return generate_password_hash(password)
 
 def check_password(password):
     return check_password_hash(pw_hash, password)
